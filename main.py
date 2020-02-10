b717ef7e54a88d388fe0e5da2e8a4135e617a5d7
Max Myslivets <by.mikky1206@gmail.com>
2020-02-10 12:11:13 +0300

    Update
    Sort to derictory, add volume button, add wave & background, fix any bags

--- main.py
+++ main.py
@@ -2,253 +2,51 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
+from kivy.core.audio import SoundLoader
+
+
 from kivy.core.window import Window
-
-
 Window.size = (300, 500)
 
 
-Builder.load_string("""
-
-# Icons for button
-# New game 'restart'
-# Next player 'skip-next'
-# Exit 'logout' or 'close-circle' (-outline)
-# The end 'presentation'
-# start 'run'
-# add player 'face-recognition' or 'creation'
-# delete player 'close'
-
-
-#:set color_shadow [0, 0, 0, .2980392156862745]
-
-
-<MyTextInput@MDTextFieldRound>
-    normal_color: color_shadow
-    active_color: color_shadow
-
-
-<Home>
-
-    n_players: n_players
-
-    canvas.before:
-        Rectangle:
-            pos: self.pos
-            size: self.size
-            source: 'AppBackground_2.jpg'
-
-    BoxLayout:
-        orientation: 'vertical'
-
-        MDIconButton:
-            pos_hint: {'right': 1}
-            icon: 'close'
-            on_release: app.stop()
-
-        BoxLayout:
-            size_hint_y: .3
-
-            Label:
-                font_size: '40sp'
-                text_size: self.size
-                halign: 'center'
-                valign: 'middle'
-                text: 'Чепуха'
-            
-        BoxLayout:
-            orientation: 'vertical'
-            padding: 10
-            spacing: 20
-            
-            MyTextInput:
-                id: n_players
-                size_hint_x: .9
-                pos_hint: {'center_x': .5}
-                icon_left: 'face-recognition'
-                icon_right: ''
-                hint_text: 'Player 1'
-            
-            MyTextInput:
-                id: player_2
-                size_hint_x: .9
-                pos_hint: {'center_x': .5}
-                icon_left: 'face-recognition'
-                icon_right: ''
-                hint_text: 'Player 2'
-
-            BoxLayout:
-                size_hint_y: .2
-            
-            MDFillRoundFlatButton:
-                text: 'Старт'
-                size_hint: .9, .1
-                pos_hint: {'center_x': .5, 'center_y': .5}
-                on_release: root.first_game_screen(n_players.text)
-                on_release: root.manager.current = 'NextGameScreen'
-            
-            BoxLayout:
-                size_hint_y: .2
-
-
-<NextGameScreen>:
-
-    quests_data: quests_data
-    answers_data: answers_data
-    quests_label: quests_label
-
-    canvas.before:
-        Rectangle:
-            pos: self.pos
-            size: self.size
-            source: 'AppBackground_2.jpg'
-
-    BoxLayout:
-        orientation: 'vertical'
-
-        AnchorLayout:
-            size_hint_y: .5
-
-            BoxLayout:
-
-                FloatLayout:
-
-                    MDFillRoundFlatButton:
-                        pos_hint: {'center_x': .5, 'center_y': .5}
-                        size_hint_x: .8
-                        text: 'Новая игра'
-
-                        on_release: app.clear_var()
-                        on_release: root.manager.current = 'Home'
-
-                FloatLayout:
-
-                    MDIconButton:
-                        pos_hint: {'right': 1, 'top': 1}
-                        valign: 'middle'
-                        icon: 'close'
-
-                        on_release: app.stop()
-
-        Label:
-            size_hint_y: .5
-            id: quests_label
-            text: 'Начните историю...'
-
-        BoxLayout:
-            orientation: 'vertical'
-            padding: 20,0,20,0
-
-            TextInput:
-                id: answers_data
-                hint_text: 'Введите ответ'
-
-        BoxLayout:
-            orientation: 'vertical'
-            padding: 20,5,20,0
-
-            TextInput:
-                id: quests_data
-                hint_text: 'Введите вопрос'
-        
-        FloatLayout:
-            MDFillRoundFlatButton:
-                pos_hint: {'center_x': .5, 'top': .9}
-                # icon: 'skip-next'
-                text: 'Передать ход'
-                size_hint_y: .3
-                on_release: root.add_data(answers_data.text, quests_data.text, quests_label.text)
-        
-        FloatLayout:
-            MDFillRoundFlatButton:
-                pos_hint: {'center_x': .5, 'center_y': .5}
-                size_hint_x: .8
-                text: 'Закончить'
-                size_hint_y: .3
-                on_release: root.end()
-                on_release: root.manager.current = 'TheEndScreen'
-                on_release: root.add_data(answers_data.text, quests_data.text, quests_label.text)
-            
-
-<TheEndScreen>:
-
-    end_label: end_label
-
-    canvas.before:
-        Rectangle:
-            pos: self.pos
-            size: self.size
-            source: 'AppBackground_2.jpg'
-
-    BoxLayout:
-        orientation: 'vertical'
-
-        FloatLayout:
-            size_hint_y: .1
-
-            MDIconButton:
-                pos_hint: {'right': 1, 'top': 1}
-                valign: 'middle'
-                icon: 'close'
-
-                on_release: app.stop()
-
-        BoxLayout:
-            padding: 20,20,20,20
-            TextInput:
-                id: end_label
-        
-        BoxLayout:
-            orientation: 'vertical'
-            size_hint_y: .3
-
-            FloatLayout:
-                MDFillRoundFlatButton:
-                    pos_hint: {'center_x': .5, 'center_y': .5}
-                    text: 'Показать историю'
-                    on_release: root.update_answers()
-            
-            FloatLayout:
-                MDFillRoundFlatButton:
-                    pos_hint: {'center_x': .5, 'center_y': .5}
-                    text: 'Сыграть ещё раз'
-                    on_release: app.clear_var()
-                    on_release: root.manager.current = 'Home'
-
-""")
-
-
-players, quests, answers, = [], [], []
+sound = SoundLoader.load('media/wave.mp3')
+sound.loop = True
+sound.play()
 
 
 class Home(Screen):
 
-    def first_game_screen(self, numb_players):
+    with open("kv/Home.kv", encoding='utf8') as HomeKV:
+        Builder.load_string(HomeKV.read())
+
+    def first_game_screen(self, player_1, player_2):
         
-        players = (numb_players.split(' '))
-        print(players)
+        ChepuhaApp().players.append(player_1)
+        ChepuhaApp().players.append(player_2)
+
+        NextGameScreen().start_quests_label()
 
 
 class NextGameScreen(Screen):
+
+    with open("kv/NextGameScreen.kv", encoding='utf8') as NextGameScreenKV:
+        Builder.load_string(NextGameScreenKV.read())
 
     quests_data = ObjectProperty()
     answers_data = ObjectProperty()
     quests_label = ObjectProperty()
 
+    def start_quests_label(self):
+        self.quests_label.text = ChepuhaApp().players[1] + ', начните историю...'
+
     def add_data(self, add_answers, add_quests, quests_label):
 
-        answers.append(add_answers)
-        quests.append(add_quests)
-        
-        # XXX
-        print(players)
-        
-        self.quests_label.text = '' + add_quests
+        ChepuhaApp().answers.append(add_answers)
+        ChepuhaApp().quests.append(add_quests)
 
-        # FIXME
-        #self.quests_label.text = players[0] + ', ' + add_quests
-        #players.append(players[0])
-        #players.remove(players[0])
+        self.quests_label.text = ChepuhaApp().players[0] + ', ' + add_quests
+        ChepuhaApp().players.append(ChepuhaApp().players[0])
+        ChepuhaApp().players.remove(ChepuhaApp().players[0])
 
         self.answers_data.text = ''
         self.quests_data.text = ''
@@ -260,18 +58,23 @@ class NextGameScreen(Screen):
 
 class TheEndScreen(Screen):
 
+    with open("kv/TheEndScreen.kv", encoding='utf8') as TheEndScreenKV:
+        Builder.load_string(TheEndScreenKV.read())
+
     end_label = ObjectProperty()
 
     def update_answers(self):
 
         ans = ''
-        for i in range(len(answers)):
-            ans += answers[i] + '\n' + quests[i] + '\n'
+        for i in range(len(ChepuhaApp().answers)):
+            ans += ChepuhaApp().answers[i] + '\n' + ChepuhaApp().quests[i] + '\n'
 
         self.end_label.text = ans
 
 
 class ChepuhaApp(MDApp):
+
+    players, quests, answers = [], [], []
 
     def build(self):
 
@@ -286,8 +89,19 @@ class ChepuhaApp(MDApp):
         return sm
     
     def clear_var(self):
-        pass
-        #players, quests, answers, = [1, 2, 456], [], []
+        
+        self.players.clear()
+        self.quests.clear()
+        self.answers.clear()
+    
+    volume_btn = ObjectProperty()
+    
+    def off_volume(self):
+
+        if sound.volume: sound.volume = 0
+        else: sound.volume = 1
+
+        #self.volume_btn.icon = 'volume-high'
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,32 +1,82 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
 
-Builder.load_string('''
 
-<testclass>:
-    padding: 20,0,0,0
+Builder.load_string("""
 
-    FloatLayout:
-        MDFillRoundFlatButton:
-            size_hint_x: .5
-            pos_hint: {'center_x': .5, 'center_y': .5}
-            text: 'New Game'
-        
-    FloatLayout:
-        MDIconButton:
-            pos_hint: {'right': 1, 'center_y': .5}
-            icon: 'close'
+<Home>:
 
-''')
+    input_1: input_1
 
-class testclass(BoxLayout):
+    BoxLayout:
+        TextInput:
+            id: input_1
+        Button:
+            text: 'Next screen'
+            on_release: root.label_update(input_1.text)
+            on_release: root.manager.current = 'NextScreen'
+
+
+<NextScreen>:
+
+    label_1: label_1
+
+    BoxLayout:
+        Label:
+            id: label_1
+            text: 'qwerty'
+        Button:
+            text: 'The End screen'
+            on_release: root.manager.current = 'TheEndScreen'
+
+
+<TheEndScreen>:
+
+    Button:
+        text: 'Home'
+        on_release: app.clear_input()
+        on_release: root.manager.current = 'Home'
+
+""")
+
+
+class Home(Screen):
+
+    input_1 = ObjectProperty()
+    
+    def label_update(self, input_1):
+
+        NextScreen().label_1.text = input_1
+
+
+class NextScreen(Screen):
+    
+    label_1 = ObjectProperty(rebind=True)
+
+
+class TheEndScreen(Screen):
     pass
 
-class Example(MDApp):
+
+class MyApp(MDApp):
 
     def build(self):
-        return testclass()
+
+        self.theme_cls.theme_style = "Dark"
+
+        sm = ScreenManager()
+        sm.add_widget(Home(name='Home'))
+        sm.add_widget(NextScreen(name='NextScreen'))
+        sm.add_widget(TheEndScreen(name='TheEndScreen'))
+
+        return sm
+    
+    def clear_input(self):
+
+        Home().input_1.text = ''
 
 
-Example().run()
+
+MyApp().run()
