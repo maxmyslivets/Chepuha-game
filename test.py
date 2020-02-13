50717b5b17f4f2bafbaa5fd48632111eab1e083a
Max Myslivets <by.mikky1206@gmail.com>
2020-02-13 13:42:16 +0300

    Пофиксил поля вывода
    Исправлено:
    * На конечном экране автоматический вывод истории в Label, который в Scrollbar. На данном этапе вывод осуществляется в TextInput
    * В Label для вывода текста вопроса при попытке отображения слишком большого количества символов, label изменяется в высоте, но при попытке вместить >3 строк, label залазит на другие объекты.

--- test.py
+++ test.py
@@ -4,59 +4,11 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 
 
-Builder.load_string("""
-
-<Home>:
-
-    input_1: input_1
-
-    BoxLayout:
-        TextInput:
-            id: input_1
-        Button:
-            text: 'Next screen'
-            on_release: root.label_update(input_1.text)
-            on_release: root.manager.current = 'NextScreen'
-
-
-<NextScreen>:
-
-    label_1: label_1
-
-    BoxLayout:
-        Label:
-            id: label_1
-            text: 'qwerty'
-        Button:
-            text: 'The End screen'
-            on_release: root.manager.current = 'TheEndScreen'
-
-
-<TheEndScreen>:
-
-    Button:
-        text: 'Home'
-        on_release: app.clear_input()
-        on_release: root.manager.current = 'Home'
-
-""")
+Builder.load_string("""#:include test.kv""")
 
 
 class Home(Screen):
 
-    input_1 = ObjectProperty()
-    
-    def label_update(self, input_1):
-
-        NextScreen().label_1.text = input_1
-
-
-class NextScreen(Screen):
-    
-    label_1 = ObjectProperty(rebind=True)
-
-
-class TheEndScreen(Screen):
     pass
 
 
@@ -68,15 +20,8 @@ class MyApp(MDApp):
 
         sm = ScreenManager()
         sm.add_widget(Home(name='Home'))
-        sm.add_widget(NextScreen(name='NextScreen'))
-        sm.add_widget(TheEndScreen(name='TheEndScreen'))
 
         return sm
-    
-    def clear_input(self):
-
-        Home().input_1.text = ''
-
 
 
 MyApp().run()
